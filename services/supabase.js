4ba0d856492f4df15c6e9d23e37d928a558f4bb6
Rafael Walder <rafaelwalder99@gmail.com>
2026-06-02 07:30:09 +0300

    Fixed supabase server

--- services/supabase.js
+++ services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ahdplrfikrisebpahryl.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZHBscmZpa3Jpc2VicGFocnlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0ODY0NDQsImV4cCI6MjA5NTA2MjQ0NH0.383yar8f5gcxkuzDF23LV61c1BOIfHNNjYvXD2QBlQk";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export default supabase;
